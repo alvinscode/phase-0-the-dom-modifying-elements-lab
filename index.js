2a8422e07d4162368a2397506c0383c3831ce5fb
Alvin Li <aflip24@gmail.com>
2023-02-16 23:20:12 -0600

    finished DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Alvin is the champion";
+element.appendChild(newHeader);
